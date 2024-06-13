6d9bbace64c6145ecbd881f165a1c896e7d60d7d
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-06-13 16:50:42 -0300

    aula lista de tarefas

---
+++ script.js
@@ -0,0 +1,45 @@
+
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = [];
+
+function renderTarefas (){
+    listElement.innerHTML = "";
+
+    tarefas.map((todo) => {
+        let liElement = document.createElement("li")
+        let tarefaText = document.createTextNode(todo)
+
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#")
+
+        let linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
+
+        liElement.appendChild(tarefaText);
+        liElement.append(linkElement)
+        listElement.appendChild(liElement)
+
+
+       
+    })
+}
+
+function adicionarTarefas(){
+    if(inputElement.value === ''){
+        alert("Digite alguma tarefa");
+        return false
+    }else{
+        let novaTarefa = inputElement.value;
+
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+
+        renderTarefas();
+    }
+}
+
+
+buttonElement.onclick= adicionarTarefas;
