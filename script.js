2d86190542f10690bf9d7d7f26510a3a5efb57f1
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-06-14 10:35:43 -0300

    aula finalizada

--- script.js
+++ script.js
@@ -1,49 +1,81 @@
-
 let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
-let tarefas = [];
-
-function renderTarefas (){
-    listElement.innerHTML = "";
-
-    tarefas.map((todo) => {
-        let liElement = document.createElement("li")
-        let tarefaText = document.createTextNode(todo)
-
-        let linkElement = document.createElement("a");
-        linkElement.setAttribute("href", "#")
-
-        let linkText = document.createTextNode("Excluir")
-        linkElement.appendChild(linkText)
-
-        liElement.appendChild(tarefaText);
-        liElement.append(linkElement)
-        listElement.appendChild(liElement)
 
 
-       
-    })
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
+
+function renderTarefas() {
+  listElement.innerHTML = "";
+
+  tarefas.map((todo) => {
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(todo);
+
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+
+    let linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
+
+    liElement.appendChild(tarefaText);
+    liElement.append(linkElement);
+    listElement.appendChild(liElement);
+  });
 }
+
+renderTarefas()
+
+function renderTarefas() {
+  listElement.innerHTML = "";
+
+  tarefas.map((todo) => {
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(todo);
+
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+
+    let linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
+
+    let posicao = tarefas.indexOf(todo);
+
+    linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+
+    liElement.appendChild(tarefaText);
+    liElement.appendChild(linkElement);
+    listElement.appendChild(liElement);
+  });
+}
+
 
 function adicionarTarefas(){
     if(inputElement.value === ''){
-        alert("Digite alguma tarefa");
-        return false
+      alert("Digite alguma tarefa");
+      return false;
     }else{
-        let novaTarefa = inputElement.value;
-
-        tarefas.push(novaTarefa);
-        inputElement.value = '';
-
-        renderTarefas();
+      let novaTarefa = inputElement.value;
+  
+      tarefas.push(novaTarefa);
+      inputElement.value = '';
+  
+      renderTarefas();
+      salvarDados()
+  
     }
+  
+  }
+
+buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao) {
+  tarefas.splice(posicao, 1);
+  renderTarefas();
+  salvarDados()
 }
 
-
-function deletarTarefa(){
-    alert("teste delete")
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas))
 }
-
-buttonElement.onclick= adicionarTarefas;
